677940ab330595b89dae24e9700a0ea42e8e94aa
BeatrizG <beatrizgarciaang0@gmail.com>
2023-01-27 19:46:14 -0300

    Se agrega proyecto con parte responsive

---
+++ README.md
@@ -0,0 +1,46 @@
+# Counter
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+
+
+## Overview
+
+### The challenge
+
+ Increase, reset or decrease the count by clicking the buttons.
+
+### Screenshots
+
+![](./images/screen1.png)
+
+
+![](./images/screen2.png)
+
+
+
+![](./images/screen3.png)
+
+
+### Links
+
+- Challenge URL: https://tinyurl.com/y2yb3vb5
+- Live Site URL: https://beatrizang.github.io/counter/
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Vanilla JS
+
+
+
+
---
+++ index.js
@@ -0,0 +1,32 @@
+let people = [
+    {
+        "name":"Susan Smith",
+        "job":"Web Developer",
+        "description":"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        "image":"person-1.jpg"
+    },
+    {
+        "name":"Anna Johnson",
+        "job":"WEB DESIGNER",
+        "description":"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal",
+        "image":"person-2.jpg"
+    },
+    {
+        "name":"Peter Jones",
+        "job":"INTERN",
+        "description":"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+        "image":"person-3.jpg"
+    },
+    {
+        "name":"Bill Anderson",
+        "job":"THE BOSS",
+        "description":"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+        "image":"person-4.jpg"
+    }
+];
+
+let index = 0;
+
+function mostrar(){
+    console.log(people[index].name);
+}
