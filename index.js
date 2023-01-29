6bc4f148105728c70891828169d1899072cd50aa
BeatrizG <beatrizgarciaang0@gmail.com>
2023-01-28 21:09:55 -0300

    Se agrega funcion surprise

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ let people = [
     },
     {
         "name":"Anna Johnson",
-        "job":"WEB DESIGNER",
+        "job":"Web Designer",
         "description":"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal",
         "image":"person-2.png"
     },
     {
         "name":"Peter Jones",
-        "job":"INTERN",
+        "job":"Intern",
         "description":"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
         "image":"person-3.png"
     },
     {
         "name":"Bill Anderson",
-        "job":"THE BOSS",
+        "job":"The Boss",
         "description":"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
         "image":"person-4.png"
     }
@@ -29,6 +29,7 @@ let index = 0;
 
 document.getElementById("button__left").addEventListener('click',mostrarAnterior);
 document.getElementById("button__right").addEventListener('click',mostrarSiguiente);
+document.getElementById("button__surprise").addEventListener('click',sorpresa);
 
 let nameP = document.getElementById("card__name");
 let job = document.getElementById("card__job");
@@ -48,6 +49,11 @@ function mostrarAnterior(){
     if(index <0){
         index = people.length - 1;
     }
+    mostrar(index);
+}
+
+function sorpresa(){
+    index = Math.floor(Math.random() * (people.length));
     mostrar(index);
 }
 
